c1e65a0416ef7d073b097badc531de9da258f401
Eugene Yang <eugene2528@gmail.com>
2025-01-14 15:40:51 -0500

    update requirement and cookie to tag along

--- page_utils.py
+++ page_utils.py
@@ -132,9 +132,10 @@ def toggle_button(label: str, key: str, default_open: bool=False, small: bool=Tr
     return st.session_state[key]
 
 def _get_session_id():
-    if 'ajs_anonymous_id' not in st.context.cookies:
+    if '_streamlit_xsrf' not in st.context.cookies:
+        st.write('<script type="text/javascript">location.reload();</script>')
         return None
-    return st.context.cookies['ajs_anonymous_id']
+    return st.context.cookies['_streamlit_xsrf']
 
 
 def _generate_salt(length: int=16):
